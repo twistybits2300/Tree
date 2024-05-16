2dcefc482943c9499654ca7c70360d719f2b1246
twistybits2300 <twistybits2300@protonmail.com>
2024-05-16 05:39:23 -0700

    Add BinarySearchTree.insert(_:)

--- Sources/Tree/BinarySearchTree.swift
+++ Sources/Tree/BinarySearchTree.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// A value semantics implementation of a binary search tree data structure.
-public struct BinarySearchTree<T> {
+public struct BinarySearchTree<T: Comparable> {
     /// The root node in the tree. Defaults to `nil`.
     public private(set) var root: TreeNode<T>?
     
@@ -10,5 +10,26 @@ public struct BinarySearchTree<T> {
     /// - Parameter root: The tree's root. Defaults to `nil`.
     public init(root: TreeNode<T>? = nil) {
         self.root = root
+    }
+    
+    // MARK: - API
+    /// Inserts the given `value` into the tree.
+    /// - Parameter value: The value to be inserted.
+    public mutating func insert(_ value: T) {
+        root = insert(from: root, value: value)
+    }
+    
+    private func insert(from node: TreeNode<T>?, value: T) -> TreeNode<T> {
+        guard let node = node else {
+            return TreeNode(value: value)
+        }
+
+        if value < node.value {
+            node.set(left: insert(from: node.left, value: value))
+        } else {
+            node.set(right: insert(from: node.right, value: value))
+        }
+
+        return node
     }
 }
--- Tests/TreeTests/BinarySearchTreeTests.swift
+++ Tests/TreeTests/BinarySearchTreeTests.swift
@@ -5,7 +5,7 @@ import os
 /// Validation of `BinarySearchTree`.
 final class BinarySearchTreeTests: XCTestCase {
     private let fixture = TreeFixture()
-
+    
     /// Validates that `init()` sets the `root` node to `nil`.
     func test_init() throws {
         let sut = fixture.makeEmptyBinarySearchTreeSUT()
@@ -18,14 +18,85 @@ final class BinarySearchTreeTests: XCTestCase {
         let sut = fixture.makeBinarySearchTreeSUT(value: rootValue)
         XCTAssertEqual(sut.root?.value, rootValue)
     }
+
+    /// Validates that `insert(_ value:)` works as expected when inserting into an empty tree.
+    func test_insert_into_empty_tree() throws {
+        let expectedValue = 123
+
+        var sut = fixture.makeEmptyBinarySearchTreeSUT()
+        XCTAssertNil(sut.root)
+
+        sut.insert(expectedValue)
+        XCTAssertNotNil(sut.root)
+        XCTAssertEqual(sut.root?.value, expectedValue)
+    }
+
+    /// Validates that `insert(_ value:)` works as expected when inserting
+    /// consecutive values into a non-empty tree.
+    func test_insert_all_right_children() throws {
+        var sut = fixture.makeEmptyBinarySearchTreeSUT()
+
+        var expectedArray = [Int]()
+        for value in fixture.numbers {
+            sut.insert(value)
+            expectedArray.append(value)
+        }
+
+        XCTAssertNotNil(sut.root)
+
+        var currentNode = sut.root
+        var traversed = [Int]()
+        while currentNode != nil {
+            if let value = currentNode?.value {
+                traversed.append(value)
+            }
+            currentNode = currentNode?.right
+        }
+        XCTAssertEqual(traversed, expectedArray)
+    }
+
+    /*
+    /// Validates that `insert(_ value:)` properly creates a binary search tree when given
+    /// a series of numbers.
+    func test_insert() throws {
+        let expectedArray = fixture.treeNumbers.sorted()
+        let sut = fixture.makeBinarySearchTreeNumbersSUT()
+
+        XCTAssertNotNil(sut.root)
+
+        var traversed = [Int]()
+        let root = try XCTUnwrap(sut.root)
+        root.traverseInOrder { traversed.append($0) }
+
+        XCTAssertEqual(traversed, expectedArray)
+    }
+    */
 }
 
 extension TreeFixture {
+    var treeNumbers: [Int] {
+        [40, 18, 77, 1, 20, 70, 105, 0, 25, 45, 88]
+    }
+    
     func makeEmptyBinarySearchTreeSUT() -> BinarySearchTree<Int> {
         BinarySearchTree()
     }
     
     func makeBinarySearchTreeSUT(value: Int) -> BinarySearchTree<Int> {
         BinarySearchTree(root: makeNodeSUT(value))
+    }
+
+    func makeBinarySearchTreeNumbersSUT() -> BinarySearchTree<Int> {
+        makeBinarySearchTreeSUT(array: treeNumbers)
+    }
+    
+    func makeBinarySearchTreeSUT(array: [Int]) -> BinarySearchTree<Int> {
+        var sut = makeEmptyBinarySearchTreeSUT()
+
+        for value in array {
+            sut.insert(value)
+        }
+
+        return sut
     }
 }
