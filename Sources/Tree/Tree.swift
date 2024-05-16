c4dd43027ff61c0a02f3338db803796ca963bf01
twistybits2300 <twistybits2300@protonmail.com>
2024-05-16 04:03:12 -0700

    Add Tree.init(root:)

--- Sources/Tree/Tree.swift
+++ Sources/Tree/Tree.swift
@@ -1,2 +1,14 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+/// A reference semantics implementation of a tree data structure.
+public final class Tree<T> {
+    /// The root node in the tree. Defaults to `nil`.
+    public private(set) var root: TreeNode<T>?
+    
+    // MARK: - Initialization
+    /// Initializes using the given `root` node.
+    /// - Parameter root: The tree's root. Defaults to `nil`.
+    public init(root: TreeNode<T>? = nil) {
+        self.root = root
+    }
+}
--- Tests/TreeTests/TreeTests.swift
+++ Tests/TreeTests/TreeTests.swift
@@ -1,7 +1,22 @@
 import XCTest
 @testable import Tree
 
+/// Validation of `Tree`.
 final class TreeTests: XCTestCase {
+    private let fixture = TreeFixture()
+    
+    /// Validates that `init()` sets the `root` node to `nil`.
+    func test_init() throws {
+        let sut = fixture.makeEmptyTreeSUT()
+        XCTAssertNil(sut.root)
+    }
+    
+    /// Validates that `init(root:)` correctly captures the given `root` node.
+    func test_initRoot() throws {
+        let rootValue = fixture.randomNumber
+        let sut = fixture.makeTreeSUT(value: rootValue)
+        XCTAssertEqual(sut.root?.value, rootValue)
+    }
 }
 
 struct TreeFixture {
@@ -18,5 +33,13 @@ struct TreeFixture {
     
     func makeNodeSUT(_ value: Int) -> TreeNode<Int> {
         TreeNode(value: value)
+    }
+    
+    func makeEmptyTreeSUT() -> Tree<Int> {
+        Tree()
+    }
+    
+    func makeTreeSUT(value: Int) -> Tree<Int> {
+        Tree(root: makeNodeSUT(value))
     }
 }
